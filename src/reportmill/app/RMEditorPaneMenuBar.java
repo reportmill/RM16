abe2d51f260f5117819179a05acf4082918ef52d
Jeff Martin <jeff@reportmill.com>
2020-02-22 13:21:53 -0600

    Moved TextEditor, SpellCheck, SpellCheckPanel to SnapKit from RMDraw.

--- src/reportmill/app/RMEditorPaneMenuBar.java
+++ src/reportmill/app/RMEditorPaneMenuBar.java
@@ -1,7 +1,7 @@
 package reportmill.app;
 import rmdraw.app.*;
 import reportmill.apptools.RMTableRowTool;
-import rmdraw.graphics.RMTextEditor;
+import snap.text.TextEditor;
 import snap.gfx.HPos;
 import snap.util.ClassUtils;
 import snap.util.Prefs;
@@ -30,8 +30,8 @@ public class RMEditorPaneMenuBar extends EditorPaneMenuBar {
     protected void initUI()
     {
         // Configure CheckSpellingAsYouTypeMenuItem and HyphenateTextMenuItem
-        setViewValue("CheckSpellingAsYouTypeMenuItem", RMTextEditor.isSpellChecking);
-        setViewValue("HyphenateTextMenuItem", RMTextEditor.isHyphenating());
+        setViewValue("CheckSpellingAsYouTypeMenuItem", TextEditor.isSpellChecking);
+        setViewValue("HyphenateTextMenuItem", TextEditor.isHyphenating());
     }
 
     /**
@@ -132,24 +132,24 @@ public class RMEditorPaneMenuBar extends EditorPaneMenuBar {
             RMEditorPaneUtils.openSample("Jar:/reportmill/examples/Sales.xml");
 
         // Handle Edit menu items
-        if(anEvent.equals("UndoMenuItem") || anEvent.equals("UndoButton")) editor.undo();
-        if(anEvent.equals("RedoMenuItem") || anEvent.equals("RedoButton")) editor.redo();
-        if(anEvent.equals("CutMenuItem") || anEvent.equals("CutButton")) editor.cut();
-        if(anEvent.equals("CopyMenuItem") || anEvent.equals("CopyButton")) editor.copy();
-        if(anEvent.equals("PasteMenuItem") || anEvent.equals("PasteButton")) editor.paste();
-        if(anEvent.equals("SelectAllMenuItem")) editor.selectAll();
-        if(anEvent.equals("CheckSpellingMenuItem")) SpellCheckPanel.getShared().show(editor);
+        if (anEvent.equals("UndoMenuItem") || anEvent.equals("UndoButton")) editor.undo();
+        if (anEvent.equals("RedoMenuItem") || anEvent.equals("RedoButton")) editor.redo();
+        if (anEvent.equals("CutMenuItem") || anEvent.equals("CutButton")) editor.cut();
+        if (anEvent.equals("CopyMenuItem") || anEvent.equals("CopyButton")) editor.copy();
+        if (anEvent.equals("PasteMenuItem") || anEvent.equals("PasteButton")) editor.paste();
+        if (anEvent.equals("SelectAllMenuItem")) editor.selectAll();
+        if (anEvent.equals("CheckSpellingMenuItem")) EditorUtils.checkSpelling(editor);;
 
         // Edit -> CheckSpellingAsYouTypeMenuItem
-        if(anEvent.equals("CheckSpellingAsYouTypeMenuItem")) {
-            RMTextEditor.isSpellChecking = anEvent.getBooleanValue();
-            Prefs.get().set("SpellChecking", RMTextEditor.isSpellChecking);
+        if (anEvent.equals("CheckSpellingAsYouTypeMenuItem")) {
+            TextEditor.isSpellChecking = anEvent.getBooleanValue();
+            Prefs.get().set("SpellChecking", TextEditor.isSpellChecking);
             editor.repaint();
         }
 
         // Edit -> HyphenateTextMenuItem
-        if(anEvent.equals("HyphenateTextMenuItem")) {
-            RMTextEditor.setHyphenating(anEvent.getBooleanValue());
+        if (anEvent.equals("HyphenateTextMenuItem")) {
+            TextEditor.setHyphenating(anEvent.getBooleanValue());
             editor.repaint();
         }
 
@@ -178,66 +178,66 @@ public class RMEditorPaneMenuBar extends EditorPaneMenuBar {
             EditorUtils.setSubscript(editor);
 
         // Handle Pages menu items
-        if(anEvent.equals("AddPageMenuItem")) editor.addPage();
-        if(anEvent.equals("AddPagePreviousMenuItem")) editor.addPagePrevious();
-        if(anEvent.equals("RemovePageMenuItem")) editor.removePage();
-        if(anEvent.equals("ZoomInMenuItem")) editor.setZoomFactor(editor.getZoomFactor() + .1f);
-        if(anEvent.equals("ZoomOutMenuItem")) editor.setZoomFactor(editor.getZoomFactor() - .1f);
-        if(anEvent.equals("Zoom100MenuItem")) editor.setZoomFactor(1);
-        if(anEvent.equals("Zoom200MenuItem")) editor.setZoomFactor(2);
-        if(anEvent.equals("ZoomToggleLastMenuItem")) editor.zoomToggleLast();
-        if(anEvent.equals("ZoomToMenuItem")) epane.runZoomPanel();
+        if (anEvent.equals("AddPageMenuItem")) editor.addPage();
+        if (anEvent.equals("AddPagePreviousMenuItem")) editor.addPagePrevious();
+        if (anEvent.equals("RemovePageMenuItem")) editor.removePage();
+        if (anEvent.equals("ZoomInMenuItem")) editor.setZoomFactor(editor.getZoomFactor() + .1f);
+        if (anEvent.equals("ZoomOutMenuItem")) editor.setZoomFactor(editor.getZoomFactor() - .1f);
+        if (anEvent.equals("Zoom100MenuItem")) editor.setZoomFactor(1);
+        if (anEvent.equals("Zoom200MenuItem")) editor.setZoomFactor(2);
+        if (anEvent.equals("ZoomToggleLastMenuItem")) editor.zoomToggleLast();
+        if (anEvent.equals("ZoomToMenuItem")) epane.runZoomPanel();
 
         // Handle Shapes menu items (use name because anObj may come from popup menu)
         String name = anEvent.getName();
-        if(name.equals("GroupMenuItem")) EditorUtils.groupShapes(editor, null, null);
-        if(name.equals("UngroupMenuItem")) EditorUtils.ungroupShapes(editor);
-        if(name.equals("BringToFrontMenuItem")) EditorUtils.bringToFront(editor);
-        if(name.equals("SendToBackMenuItem")) EditorUtils.sendToBack(editor);
-        if(name.equals("MakeRowTopMenuItem")) EditorUtils.makeRowTop(editor);
-        if(name.equals("MakeRowCenterMenuItem")) EditorUtils.makeRowCenter(editor);
-        if(name.equals("MakeRowBottomMenuItem")) EditorUtils.makeRowBottom(editor);
-        if(name.equals("MakeColumnLeftMenuItem")) EditorUtils.makeColumnLeft(editor);
-        if(name.equals("MakeColumnCenterMenuItem")) EditorUtils.makeColumnCenter(editor);
-        if(name.equals("MakeColumnRightMenuItem")) EditorUtils.makeColumnRight(editor);
-        if(name.equals("MakeSameSizeMenuItem")) EditorUtils.makeSameSize(editor);
-        if(name.equals("MakeSameWidthMenuItem")) EditorUtils.makeSameWidth(editor);
-        if(name.equals("MakeSameHeightMenuItem")) EditorUtils.makeSameHeight(editor);
-        if(name.equals("SizeToFitMenuItem")) EditorUtils.setSizeToFit(editor);
-        if(name.equals("EquallySpaceRowMenuItem")) EditorUtils.equallySpaceRow(editor);
-        if(name.equals("EquallySpaceColumnMenuItem")) EditorUtils.equallySpaceColumn(editor);
-        if(name.equals("GroupInSwitchShapeMenuItem")) RMEditorUtils.groupInSwitchShape(editor);
-        if(name.equals("GroupInScene3DMenuItem")) EditorUtils.groupInScene3D(editor);
-        if(name.equals("MoveToNewLayerMenuItem")) EditorUtils.moveToNewLayer(editor);
-        if(name.equals("CombinePathsMenuItem")) EditorUtils.combinePaths(editor);
-        if(name.equals("SubtractPathsMenuItem")) EditorUtils.subtractPaths(editor);
-        if(name.equals("ConvertToImageMenuItem")) EditorUtils.convertToImage(editor);
+        if (name.equals("GroupMenuItem")) EditorUtils.groupShapes(editor, null, null);
+        if (name.equals("UngroupMenuItem")) EditorUtils.ungroupShapes(editor);
+        if (name.equals("BringToFrontMenuItem")) EditorUtils.bringToFront(editor);
+        if (name.equals("SendToBackMenuItem")) EditorUtils.sendToBack(editor);
+        if (name.equals("MakeRowTopMenuItem")) EditorUtils.makeRowTop(editor);
+        if (name.equals("MakeRowCenterMenuItem")) EditorUtils.makeRowCenter(editor);
+        if (name.equals("MakeRowBottomMenuItem")) EditorUtils.makeRowBottom(editor);
+        if (name.equals("MakeColumnLeftMenuItem")) EditorUtils.makeColumnLeft(editor);
+        if (name.equals("MakeColumnCenterMenuItem")) EditorUtils.makeColumnCenter(editor);
+        if (name.equals("MakeColumnRightMenuItem")) EditorUtils.makeColumnRight(editor);
+        if (name.equals("MakeSameSizeMenuItem")) EditorUtils.makeSameSize(editor);
+        if (name.equals("MakeSameWidthMenuItem")) EditorUtils.makeSameWidth(editor);
+        if (name.equals("MakeSameHeightMenuItem")) EditorUtils.makeSameHeight(editor);
+        if (name.equals("SizeToFitMenuItem")) EditorUtils.setSizeToFit(editor);
+        if (name.equals("EquallySpaceRowMenuItem")) EditorUtils.equallySpaceRow(editor);
+        if (name.equals("EquallySpaceColumnMenuItem")) EditorUtils.equallySpaceColumn(editor);
+        if (name.equals("GroupInSwitchShapeMenuItem")) RMEditorUtils.groupInSwitchShape(editor);
+        if (name.equals("GroupInScene3DMenuItem")) EditorUtils.groupInScene3D(editor);
+        if (name.equals("MoveToNewLayerMenuItem")) EditorUtils.moveToNewLayer(editor);
+        if (name.equals("CombinePathsMenuItem")) EditorUtils.combinePaths(editor);
+        if (name.equals("SubtractPathsMenuItem")) EditorUtils.subtractPaths(editor);
+        if (name.equals("ConvertToImageMenuItem")) EditorUtils.convertToImage(editor);
 
         // Handle Tools menu items
-        if(anEvent.equals("InspectorMenuItem")) epane.getInspectorPanel().setVisible(-1);
-        if(anEvent.equals("ColorPanelMenuItem")) epane.getAttributesPanel().setVisibleName(AttributesPanel.COLOR);
-        if(anEvent.equals("FormatPanelMenuItem")) epane.getAttributesPanel().setVisibleName(AttributesPanel.FORMAT);
-        if(anEvent.equals("KeysPanelMenuItem")) epane.getAttributesPanel().setVisibleName(AttributesPanel.KEYS);
+        if (anEvent.equals("InspectorMenuItem")) epane.getInspectorPanel().setVisible(-1);
+        if (anEvent.equals("ColorPanelMenuItem")) epane.getAttributesPanel().setVisibleName(AttributesPanel.COLOR);
+        if (anEvent.equals("FormatPanelMenuItem")) epane.getAttributesPanel().setVisibleName(AttributesPanel.FORMAT);
+        if (anEvent.equals("KeysPanelMenuItem")) epane.getAttributesPanel().setVisibleName(AttributesPanel.KEYS);
 
         // Handle ShowRulersMenuItem, FeedbackMenuItem, PrefsMenuItem
-        if(anEvent.equals("ShowRulersMenuItem")) epane.setShowRulers(!epane.isShowRulers());
-        if(anEvent.equals("FeedbackMenuItem")) new FeedbackPanel().showPanel(epane.getUI());
-        if(anEvent.equals("PrefsMenuItem")) new PrefsPanel().showPanel(epane.getUI());
+        if (anEvent.equals("ShowRulersMenuItem")) epane.setShowRulers(!epane.isShowRulers());
+        if (anEvent.equals("FeedbackMenuItem")) new FeedbackPanel().showPanel(epane.getUI());
+        if (anEvent.equals("PrefsMenuItem")) new PrefsPanel().showPanel(epane.getUI());
 
         // Handle SupportPageMenuItem, TutorialMenuItem, BasicAPIMenuItem, TablesMenuItem
-        if(anEvent.equals("SupportPageMenuItem")) URLUtils.openURL("http://reportmill.com/support");
-        if(anEvent.equals("TutorialMenuItem")) URLUtils.openURL("http://reportmill.com/support/tutorial.pdf");
-        if(anEvent.equals("BasicAPIMenuItem")) URLUtils.openURL("http://reportmill.com/support/BasicApi.pdf");
-        if(anEvent.equals("TablesMenuItem")) URLUtils.openURL("http://reportmill.com/support/tables.pdf");
+        if (anEvent.equals("SupportPageMenuItem")) URLUtils.openURL("http://reportmill.com/support");
+        if (anEvent.equals("TutorialMenuItem")) URLUtils.openURL("http://reportmill.com/support/tutorial.pdf");
+        if (anEvent.equals("BasicAPIMenuItem")) URLUtils.openURL("http://reportmill.com/support/BasicApi.pdf");
+        if (anEvent.equals("TablesMenuItem")) URLUtils.openURL("http://reportmill.com/support/tables.pdf");
 
         // Handle AddColumnMenuItem, SplitColumnMenuItem (from right mouse pop-up)
-        if(anEvent.equals("AddColumnMenuItem")) RMTableRowTool.addColumn(editor);
-        if(anEvent.equals("SplitColumnMenuItem")) EditorUtils.splitHorizontal(editor);
+        if (anEvent.equals("AddColumnMenuItem")) RMTableRowTool.addColumn(editor);
+        if (anEvent.equals("SplitColumnMenuItem")) EditorUtils.splitHorizontal(editor);
 
         // Handle Theme menus: StandardThemeMenuItem, LightThemeMenuItem, DarkThemeMenuItem, BlackAndWhiteThemeMenuItem
-        if(anEvent.equals("StandardThemeMenuItem")) ViewTheme.setThemeForName("Standard");
-        if(anEvent.equals("LightThemeMenuItem")) ViewTheme.setThemeForName("Light");
-        if(anEvent.equals("DarkThemeMenuItem")) ViewTheme.setThemeForName("Dark");
-        if(anEvent.equals("BlackAndWhiteThemeMenuItem")) ViewTheme.setThemeForName("BlackAndWhite");
+        if (anEvent.equals("StandardThemeMenuItem")) ViewTheme.setThemeForName("Standard");
+        if (anEvent.equals("LightThemeMenuItem")) ViewTheme.setThemeForName("Light");
+        if (anEvent.equals("DarkThemeMenuItem")) ViewTheme.setThemeForName("Dark");
+        if (anEvent.equals("BlackAndWhiteThemeMenuItem")) ViewTheme.setThemeForName("BlackAndWhite");
     }
 }
